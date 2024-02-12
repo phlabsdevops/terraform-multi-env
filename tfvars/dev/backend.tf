41a59ff3ec9042e58ed3a15cb05fe32c3329c844
phlabsdevops <harinathbeeram1999@gmail.com>
2024-02-12 11:17:54 +0530

    phlabsdevops

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "phlabs-state-dev"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "phlabs-locking-dev"
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "phlabs-state-prod"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "phlabs-locking-prod"
